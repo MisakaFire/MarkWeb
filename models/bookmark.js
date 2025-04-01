172635a4cc810457a3597443116db1b4b508aaaa
MisakaFire <misaka@whu.edu.cn>
2025-04-01 20:05:57 +0800

    Update bookmark.js

--- models/bookmark.js
+++ models/bookmark.js
@@ -8,4 +8,12 @@ const Bookmark = sequelize.define('Bookmark', {
 // 添加关联关系
 Bookmark.belongsTo(require('./category'));
 
+const Bookmark = sequelize.define('Bookmark', {
+  // ...原有字段保持不变
+  order: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+});
+
 module.exports = Bookmark;
