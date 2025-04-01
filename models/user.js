4b03e2b029481cc97254e452d047337ce40aea1b
MisakaFire <misaka@whu.edu.cn>
2025-04-01 20:32:01 +0800

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    set(value) {
+      this.setDataValue('password', bcrypt.hashSync(value, 10));
+    }
+  }
+});
+
+User.prototype.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = User;
