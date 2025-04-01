61e3278f47f4e25c1c33a632a89e7516607b3ff2
MisakaFire <misaka@whu.edu.cn>
2025-04-01 20:20:47 +0800

    Update add-order-to-bookmark.js

--- migrations/add-order-to-bookmark.js
+++ migrations/add-order-to-bookmark.js
@@ -1,1 +1,21 @@
+'use strict';
 
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Bookmarks', 'order', {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    });
+    
+    // 初始化现有数据的order值
+    await queryInterface.sequelize.query(`
+      UPDATE Bookmarks 
+      SET order = id
+      WHERE order IS NULL
+    `);
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('Bookmarks', 'order');
+  }
+};
