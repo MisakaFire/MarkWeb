bc395f86cf7b54c70ed6b7baccedb92aadc73dda
MisakaFire <misaka@whu.edu.cn>
2025-04-01 20:01:16 +0800

    Update category.js

--- models/category.js
+++ models/category.js
@@ -13,4 +13,13 @@ const Category = sequelize.define('Category', {
   }
 });
 
+// models/category.js
+Category.addHook('beforeDestroy', async (category) => {
+  // 将关联书签设置为默认分类
+  await Bookmark.update(
+    { CategoryId: 1 }, // 假设存在默认分类
+    { where: { CategoryId: category.id } }
+  );
+});
+
 module.exports = Category;
